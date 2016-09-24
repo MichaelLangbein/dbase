01c43630187c365600b583960706d0e7fb486c90
rinso86 <michael_langbein@hotmail.de>
2016-09-24 21:42:32 +0200

    Bintree besser gemacht

--- bintree.c
+++ bintree.c
@@ -1,30 +1,47 @@
 #include "bintree.h"
 
+/**
+ * Auch bintrees brauchen nicht per se numerische Schlüssel.
+ * Was an bintrees besonders ist, ist, dass sie im Gegensatz zu hashtables eigentlich
+ * immer schon sortiert sind.
+ *
+ * Damit inserts und selects aber wirklich immer nur log(n) Schritte brauchen, 
+ * muss der bintree regelmäßig gebalanced werden. 
+ */
 
 
 void freeTree(node * nextnode){
 	if(nextnode != 0){
-		freeTree(nextnode->lwr);
-		freeTree(nextnode->hgr);
+		freeTree(nextnode->left);
+		freeTree(nextnode->right);
 		free(nextnode);
 	}else{
 		free(nextnode);
 	}
 }
 
+node * makeNewNode(int key, node * parentnode){
+	node * newnode = (node*) malloc( sizeof(node) );
+	newnode->key_val = key;
+	newnode->left = 0;
+	newnode->right = 0;
+	newnode->parent = parentnode;
+	return newnode;
+}
 
-void insertInTree(int key, node * nextnode){
-	if(nextnode == 0){
 
-		nextnode = (node*) malloc( sizeof(node) );
-		nextnode->key_val = key;
-		nextnode->lwr = 0;
-		nextnode->hgr = 0;
-	}else{
-		if(key < nextnode->key_val){
-			insertInTree(key, nextnode->lwr);
+void insertInTree(int key, node * thisnode){
+	if(key < thisnode->key_val){
+		if(!thisnode->left){
+			thisnode->left = makeNewNode(key, thisnode);
 		}else{
-			insertInTree(key, nextnode->hgr);
+			insertInTree(key, thisnode->left);
+		}
+	}else{
+		if(!thisnode->right){
+			thisnode->right = makeNewNode(key, thisnode);
+		}else{
+			insertInTree(key, thisnode->right);
 		}
 	}
 }
@@ -34,9 +51,17 @@ node * findNode(int key, node * nextnode){
 		return nextnode;
 	}else{
 		if(key < nextnode->key_val){
-			findNode(key, nextnode->lwr);
+			findNode(key, nextnode->left);
 		}else{
-			findNode(key, nextnode->hgr);
+			findNode(key, nextnode->right);
 		}
 	}
 }
+
+
+
+void balanceTree(node * root){
+}
+
+
+void printSortedVals(node * root){}
--- bintree.h
+++ bintree.h
@@ -2,14 +2,21 @@
 
 
 typedef struct node {
-	struct node * lwr;
-	struct node * hgr;
+	struct node * left;
+	struct node * right;
+	struct node * parent;
 	int key_val;
 } node;
 
 
 void freeTree(node * nextnode);
 
+node * makeNewNode(int key, node * parentnode);
+
 void insertInTree(int key, node * nextnode);
 
 node * findNode(int key, node * nextnode);
+
+void balanceTree(node * root);
+
+void printSortedVals(node * root);
